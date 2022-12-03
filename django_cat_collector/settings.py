b93693a591c603aab0b99bcfcdf0f55b8140ec93
lrakoto <lova@threeohfivestudios.com>
2022-12-03 15:47:26 -0800

    update push

--- django_cat_collector/settings.py
+++ django_cat_collector/settings.py
@@ -80,8 +80,8 @@ WSGI_APPLICATION = 'django_cat_collector.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cats',
     }
 }
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,25 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Cat
+# add these lines to the imports at the top
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.http import HttpResponseRedirect
 
-# temp add Cats class
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-    
-    def __str__(self):
-        return f'What am I? {self.name}'
+class CatCreate(CreateView):
+  model = Cat
+  fields = '__all__'
+  success_url = '/cats'
 
-cats = [
-    Cat('Rufus', 'tabbycat', 'crazy cat', 103),
-    Cat('Bert', 'Calico', 'funny', 15),
-    Cat('Simba', 'lion', 'brave', 5)
-]
+class CatUpdate(UpdateView):
+  model = Cat
+  fields = ['name', 'breed', 'description', 'age']
+
+  def form_valid(self, form):
+    self.object = form.save(commit=False)
+    self.object.save()
+    return HttpResponseRedirect('/cats/' + str(self.object.pk))
+
+class CatDelete(DeleteView):
+  model = Cat
+  success_url = '/cats'
 
 # Create your views here.
 def index(request):
+    cats = list(Cat.objects.all())
     return render(request, 'index.html', { 'cats': cats })
 
 def about(request):
@@ -32,4 +38,9 @@ def blog(request):
     return render(request, 'contact.html')
 
 def cats_index(request):
+    cats = list(Cat.objects.all())
     return render(request, 'cats/index.html', { 'cats': cats })
+
+def cats_show(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/show.html', { 'cat': cat})
